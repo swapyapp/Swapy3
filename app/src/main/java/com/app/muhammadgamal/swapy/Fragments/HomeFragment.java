eb2f6247ec0fca6fcb6313100db524fff519a17e
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-10-21 20:46:13 +0200

    latest swaps on top of the list

--- app/src/main/java/com/app/muhammadgamal/swapy/Fragments/HomeFragment.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/Fragments/HomeFragment.java
@@ -37,6 +37,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
--- app/src/main/java/com/app/muhammadgamal/swapy/SwapData/SwapAdapter.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/SwapData/SwapAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +30,13 @@ public class SwapAdapter extends ArrayAdapter<SwapDetails> {
 
     public SwapAdapter(Context context, int resource, List<SwapDetails> sampleArrayList) {
         super(context, resource, sampleArrayList);
+    }
+
+    @Nullable
+    @Override
+    public SwapDetails getItem(int position) {
+        //latest swaps on top of the list
+        return super.getItem(getCount() - position - 1);
     }
 
     @NonNull
