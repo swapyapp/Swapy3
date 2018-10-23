7a1e266fdfe70b7a446194ccb3ad6d56b22b34ee
swapyapp <43712240+swapyapp@users.noreply.github.com>
2018-10-23 06:48:47 +0200

    Sent notification request to FireBase

--- app/src/main/java/com/app/muhammadgamal/swapy/Activities/ProfileActivity.java
+++ app/src/main/java/com/app/muhammadgamal/swapy/Activities/ProfileActivity.java
@@ -3,17 +3,27 @@ package com.app.muhammadgamal.swapy.Activities;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.app.muhammadgamal.swapy.Notifications.SendActivity;
 import com.app.muhammadgamal.swapy.R;
 import com.app.muhammadgamal.swapy.SwapData.SwapDetails;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,6 +34,11 @@ public class ProfileActivity extends AppCompatActivity {
     Button buttonSwapRequest;
     ProgressBar progressBar;
     private FirebaseAuth mAuth;
+    private String requestMessage;
+    Button tryButton;
+
+    //The FireBase store that will contain the map of the notifications for each user with his ID
+    private FirebaseFirestore mFireStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +46,13 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         mAuth = FirebaseAuth.getInstance();
-        String currentUserId = mAuth.getCurrentUser().getUid();
+        final String currentUserId = mAuth.getCurrentUser().getUid();
+
+        mFireStore = FirebaseFirestore.getInstance();
 
         Intent intent = getIntent();
         SwapDetails swapDetails = intent.getParcelableExtra("swapper info");
-        String swapperID = swapDetails.getSwapperID();
+        final String swapperID = swapDetails.getSwapperID();
         String swapperName = swapDetails.getSwapperName();
         String swapperEmail = swapDetails.getSwapperEmail();
         String swapperPhone = swapDetails.getSwapperPhone();
@@ -47,6 +64,10 @@ public class ProfileActivity extends AppCompatActivity {
         String swapperShiftTime = swapDetails.getSwapperShiftTime();
         String swapperTeamLeader = swapDetails.getSwapperTeamLeader();
         String swapperPreferredShift = swapDetails.getSwapperPreferredShift();
+
+
+        //set the request message
+        requestMessage = swapperName + "" +(R.string.notification_message);
 
         profileUserImg = (CircleImageView) findViewById(R.id.profileUserImg);
         if (swapperImageUrl != null){
@@ -77,15 +98,50 @@ public class ProfileActivity extends AppCompatActivity {
         textSentOrAcceptedRequest = (TextView) findViewById(R.id.textSentOrAcceptedRequest);
 
         buttonSwapRequest = (Button) findViewById(R.id.buttonSwapRequest);
-
+        buttonSwapRequest.bringToFront();
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        tryButton = findViewById(R.id.button2);
+        tryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                Map <String, Object> notificationMessage = new HashMap<>();
+                notificationMessage.put("message", requestMessage);
+                notificationMessage.put("from", currentUserId);
+                mFireStore.collection("Users/" + swapperID + "notification").add(notificationMessage).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(ProfileActivity.this,"Notification sent", Toast.LENGTH_LONG );
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
 
         if (swapperID.equals(currentUserId)){
             buttonSwapRequest.setVisibility(View.GONE);
             progressBar.setVisibility(View.GONE);
         }
 
+        buttonSwapRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
 
+                Map <String, Object> notificationMessage = new HashMap<>();
+                notificationMessage.put("message", requestMessage);
+                notificationMessage.put("from", currentUserId);
+                mFireStore.collection("Users/" + swapperID + "notification").add(notificationMessage).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(ProfileActivity.this,"Notification sent", Toast.LENGTH_LONG );
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
 
     }
 }
